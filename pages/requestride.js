91ef502e1053db421bdfda7c84520fd5c8b882bb
Deepit Chandgothia <chandgot2@grinnell.edu>
2024-03-01 22:37:11 -0600

    Updated Form UI

--- pages/requestride.js
+++ pages/requestride.js
@@ -1,84 +1,98 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import styles from '../styles/RequestRide.module.css';
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from "../styles/RequestRide.module.css";
 
 const RequestRide = () => {
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
-  const [rideType, setRideType] = useState('public');
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
-  const router = useRouter();
+	const [from, setFrom] = useState("");
+	const [to, setTo] = useState("");
+	const [rideType, setRideType] = useState("public");
+	const [date, setDate] = useState("");
+	const [time, setTime] = useState("");
+	const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const rideData = { from, to, rideType, date, time };
-  
-    // Send the ride data to the API route
-    await fetch('/api/saveRideData', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(rideData),
-    });
-  
-    // Navigate to /confirmride with query parameters
-    router.push({
-      pathname: '/mapHtml',
-      query: { ...rideData },
-    });
-  };
-  
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const rideData = { from, to, rideType, date, time };
 
-  return (
-    <>
-      <nav className={styles.navbar}>
-        <img src='../public/logo.png' alt='Ride Sharing Logo'/>
-        <div className={styles.navItems}>
-          <button onClick={() => router.push('/availableride')} className={styles.navButton}>Available Ride</button>
-          <button onClick={() => router.push('/requestride')} className={styles.navButton}>Request Ride</button>
-          <button className={styles.signOut}>Sign Out</button>
-        </div>
-      </nav>
-      <div>Request Ride </div>
-      <div className={styles.formContainer}>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <input
-            type="text"
-            value={from}
-            onChange={(e) => setFrom(e.target.value)}
-            placeholder="From"
-            className={styles.input}
-          />
-          <input
-            type="text"
-            value={to}
-            onChange={(e) => setTo(e.target.value)}
-            placeholder="To"
-            className={styles.input}
-          />
-          <select value={rideType} onChange={(e) => setRideType(e.target.value)} className={styles.select}>
-            <option value="public">Public</option>
-            <option value="private">Private</option>
-          </select>
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className={styles.input}
-          />
-          <input
-            type="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            className={styles.input}
-          />
-          <button type="submit" className={styles.submitButton}>Request Ride</button>
-        </form>
-      </div>
-    </>
-  );
-}
+		// Send the ride data to the API route
+		await fetch("/api/saveRideData", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(rideData),
+		});
+
+		// Navigate to /confirmride with query parameters
+		router.push({
+			pathname: "/mapHtml",
+			query: { ...rideData },
+		});
+	};
+
+	return (
+		<>
+			<nav className={styles.navbar}>
+				<img src='../public/logo.png' alt='Ride Sharing Logo' />
+				<div className={styles.navItems}>
+					<button
+						onClick={() => router.push("/availableride")}
+						className={styles.navButton}
+					>
+						Existing Rides
+					</button>
+					<button
+						onClick={() => router.push("/requestride")}
+						className={styles.navButton}
+					>
+						Request Ride
+					</button>
+					<button className={styles.signOut}>Sign Out</button>
+				</div>
+			</nav>
+			<div className={styles.formContainer}>
+				<form onSubmit={handleSubmit} className={styles.form}>
+					<input
+						type='text'
+						value={from}
+						onChange={(e) => setFrom(e.target.value)}
+						placeholder='From'
+						className={styles.input}
+					/>
+					<input
+						type='text'
+						value={to}
+						onChange={(e) => setTo(e.target.value)}
+						placeholder='To'
+						className={styles.input}
+					/>
+					<select
+						value={rideType}
+						onChange={(e) => setRideType(e.target.value)}
+						className={styles.select}
+					>
+						<option value='public'>Public</option>
+						<option value='private'>Private</option>
+					</select>
+					<input
+						type='date'
+						value={date}
+						onChange={(e) => setDate(e.target.value)}
+						className={styles.input}
+					/>
+					<input
+						type='time'
+						value={time}
+						onChange={(e) => setTime(e.target.value)}
+						className={styles.input}
+					/>
+					<button type='submit' className={styles.submitButton}>
+						Request Ride
+					</button>
+				</form>
+			</div>
+		</>
+	);
+};
 
 export default RequestRide;
